1656ff99188fd20e6c4b6df2331a077fa69a6721
SuugenRaj <96537338+SuugenRaj@users.noreply.github.com>
2022-01-13 01:02:32 +0800

    Add files via upload

---
+++ saverating.php
@@ -0,0 +1,13 @@
+<?php
+
+$studentid = $_POST["studentid"];
+$rating = $_POST["rating"];
+
+$conn = mysqli_connect("localhost", "root", "", "test");
+ 
+mysqli_query($conn, "INSERT INTO rating (studentid, rating) VALUES ('$studentid', '$rating')");
+
+// whatever you echo here, will be displayed in alert on user side
+echo "Saved";
+
+?>
