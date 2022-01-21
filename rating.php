cb529ebbbec70aa0504234c58537d53edd7322cb
SuugenRaj <suugenraj.venugopal@gmail.com>
2022-01-21 16:34:46 +0800

    Update rating.php

--- rating.php
+++ rating.php
@@ -61,7 +61,7 @@
 
 <?php
     // Connecting with database
-       $conn = mysqli_connect("localhost", "root", "", "test");
+       $conn = mysqli_connect("localhost", "root", "", "studFYP");
  
     // Getting all students
        $result = mysqli_query($conn, "SELECT * FROM student");
