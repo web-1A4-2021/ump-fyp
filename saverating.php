afb4ca7549cbeb8d702bfbe2207673c74d9d8834
SuugenRaj <suugenraj.venugopal@gmail.com>
2022-01-28 21:40:36 +0800

    Update saverating.php

--- saverating.php
+++ saverating.php
@@ -4,7 +4,7 @@
 $studentid = $_POST["studentid"];
 $rating = $_POST["rating"];
 
-$conn = mysqli_connect("localhost", "root", "", "studfyp");
+$conn = mysqli_connect("localhost", "root", "", "studFYP");
  
 mysqli_query($conn, "INSERT INTO rating (studentid, rating) VALUES ('$studentid', '$rating')");
 
