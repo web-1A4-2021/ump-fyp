faf5654ca7068f1c55d0c779427acf5340e20756
ainnzr <ainnazri39@gmail.com>
2022-01-28 17:20:53 +0800

    update

--- include/config.php
+++ include/config.php
@@ -3,14 +3,14 @@ $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $cleardb_server = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
 $cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1;
+$cleardb_db = substr($cleardb_url["path"],1);
 $active_group ='default';
 
 define('DB_TYPE', 'mysql');
 define('DB_HOST', $cleardb_server);
 define('DB_NAME', $cleardb_db);
 define('DB_USER', $cleardb_username);
-define('DB_HOST', $cleardb_password);
+define('DB_PASS', $cleardb_password);
 
 //! This is darwish deployment settings
 function config()
