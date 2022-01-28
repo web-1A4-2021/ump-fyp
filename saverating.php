9ea7b0747a14738b28962333c2c68424012ad5ed
SuugenRaj <suugenraj.venugopal@gmail.com>
2022-01-28 21:24:26 +0800

    Update saverating.php

--- saverating.php
+++ saverating.php
@@ -3,7 +3,7 @@
 $studentid = $_POST["studentid"];
 $rating = $_POST["rating"];
 
-$conn = mysqli_connect("localhost", "root", "", "studFYP");
+$conn = mysqli_connect("localhost", "root", "", "heroku_ea38f35ca4afbb9");
  
 mysqli_query($conn, "INSERT INTO rating (studentid, rating) VALUES ('$studentid', '$rating')");
 
